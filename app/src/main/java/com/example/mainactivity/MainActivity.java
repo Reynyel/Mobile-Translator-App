cf85d7a2cf9715b0e67b140105cd221adbae2662
Ranielle <raniellecruz65@gmail.com>
2023-08-05 14:07:56 +0800

    changed dependency to mlkit

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTxt;
+    private Button translateBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //editTxt = findViewById(R.id.translateTxt);
+        //translateBtn = findViewById(R.id.translateBtn);
     }
 }
