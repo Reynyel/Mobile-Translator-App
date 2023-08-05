05bdbb0fd37a4f7ad534211ffc281e4bca9e78f7
Ranielle <raniellecruz65@gmail.com>
2023-08-05 14:32:25 +0800

    Add text translation functionality
    In the MainActivity, added code to translate text from English to Tagalog using Google ML Kit.
    Also added indicators to check whether the translation is successful or not.

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mainactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
                 * if it's empty, a message will appear, stating it so*/
                 if(TextUtils.isEmpty(editTxt.getText().toString())){
                     Toast.makeText(MainActivity.this, "Text is empty", Toast.LENGTH_SHORT).show();
+                }else{
+                    // Build TranslatorOptions with the target language "tl" (Tagalog) and the source language "en" (English)
+                    TranslatorOptions options = new TranslatorOptions.Builder()
+                            .setTargetLanguage("tl")
+                            .setSourceLanguage("en").build();
+
+                    // Create a Translator instance based on the given options
+                    Translator translator = Translation.getClient(options);
+
+                    // Get the source language text from the edit text
+                    String sourceLang = editTxt.getText().toString();
+                    // Translate the text using the Translator instance and listen for success or failure
+                    Task<String> result = translator.translate(sourceLang).addOnSuccessListener(new OnSuccessListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            // If translation is successful, display the translated text using a toast
+                            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // If translation is successful, display the translated text using a toast
+                            Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                 }
             }
         });
