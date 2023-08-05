ee1ae85e33027856ca7613776f792bf5e0d44640
Ranielle <raniellecruz65@gmail.com>
2023-08-05 14:17:00 +0800

    add input checker

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //editTxt = findViewById(R.id.translateTxt);
-        //translateBtn = findViewById(R.id.translateBtn);
+        editTxt = findViewById(R.id.translateTxt);
+        translateBtn = findViewById(R.id.translateBtn);
+
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Checks if the edit text is empty or not
+                * if it's empty, a message will appear, stating it so*/
+                if(TextUtils.isEmpty(editTxt.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Text is empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
